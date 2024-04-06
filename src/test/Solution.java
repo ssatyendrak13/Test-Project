7e5b1a041357261424f2c5f092e8a9cd28f6839b
ssatyendrak13 <satyendra1es1ds@gmail.com>
2024-04-07 01:02:22 +0530

    name method is added in solution class

--- src/test/Solution.java
+++ src/test/Solution.java
@@ -5,5 +5,10 @@ public class Solution {
 		System.out.println("Hello world!");
 		Test obj = new Test();
 		obj.print();
+		System.out.println();
+		System.out.println(name());
+	}
+	public static String name() {
+		return "Satyendra";
 	}
 }
