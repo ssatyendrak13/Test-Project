d94018889ff432684ac8142c1b0cba9efcb3c1b1
ssatyendrak13 <satyendra1es1ds@gmail.com>
2024-04-07 00:55:31 +0530

    Test class added

--- src/test/Solution.java
+++ src/test/Solution.java
@@ -3,5 +3,7 @@ package test;
 public class Solution {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		Test obj = new Test();
+		obj.print();
 	}
 }
---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test {
+    
+    public void print(){
+        for(int i=1; i<=15; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
